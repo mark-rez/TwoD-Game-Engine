74d9bdc887427a6f402ced06f131ff6e5915ea9b
Mark <kuzmitsch33@gmail.com>
2025-05-09 17:43:12 +0200

    Refactor mouse manager

--- src/engine/input/MouseManager.java
+++ src/engine/input/MouseManager.java
@@ -2,36 +2,33 @@ package engine.input;
 
 import java.awt.event.MouseEvent;
 import java.awt.event.MouseListener;
+import java.util.EnumMap;
+import java.util.Map;
 
 public class MouseManager implements MouseListener {
 
-    private boolean leftMouseButtonDown = false;
-    private boolean rightMouseButtonDown = false;
+    private final Map<MouseButton, Boolean> buttonStates = new EnumMap<>(MouseButton.class);
+
+    public MouseManager() {
+        for (MouseButton button : MouseButton.values()) {
+            buttonStates.put(button, false);
+        }
+    }
 
     @Override
     public void mousePressed(MouseEvent e) {
         MouseButton button = MouseButton.fromCode(e.getButton());
-        setIsMouseButtonDown(button, true);
+        buttonStates.put(button, true);
     }
 
     @Override
     public void mouseReleased(MouseEvent e) {
         MouseButton button = MouseButton.fromCode(e.getButton());
-        setIsMouseButtonDown(button, false);
+        buttonStates.put(button, false);
     }
 
     public boolean isMouseButtonDown(MouseButton button) {
-        return switch (button) {
-            case LEFT -> leftMouseButtonDown;
-            case RIGHT -> rightMouseButtonDown;
-        };
-    }
-
-    private void setIsMouseButtonDown(MouseButton button, boolean isDown) {
-        switch (button) {
-            case LEFT -> leftMouseButtonDown = isDown;
-            case RIGHT -> rightMouseButtonDown = isDown;
-        }
+        return buttonStates.getOrDefault(button, false);
     }
 
     @Override public void mouseClicked(MouseEvent e) {}
