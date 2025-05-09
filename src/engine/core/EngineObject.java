517a2c87393b6f7aaa3e3ce2c5f64dd960da4ed6
Mark <kuzmitsch33@gmail.com>
2025-05-09 12:18:18 +0200

    Change position to be a point

--- src/engine/core/EngineObject.java
+++ src/engine/core/EngineObject.java
@@ -3,20 +3,26 @@ package engine.core;
 import engine.Configuration;
 
 import java.awt.*;
+import java.awt.geom.Point2D;
 
 public abstract class EngineObject {
-    protected double x, y;
+    protected Point2D.Double position;
     protected int width, height;
 
     public EngineObject(double x, double y, int width, int height) {
-        this.x = x;
-        this.y = y;
+        position = new Point2D.Double(x, y);
+        this.width = width;
+        this.height = height;
+    }
+
+    public EngineObject(Point2D.Double position, int width, int height) {
+        this.position = new Point2D.Double(position.x, position.y);
         this.width = width;
         this.height = height;
     }
 
     public boolean isOutOfBounds() {
-        return x - width < 0 || x > Configuration.FRAME_WIDTH || y - height < 0 || y > Configuration.FRAME_HEIGHT;
+        return position.x - width < 0 || position.x > Configuration.FRAME_WIDTH || position.y - height < 0 || position.y > Configuration.FRAME_HEIGHT;
     }
 
     public abstract void render(Graphics g);
